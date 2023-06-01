c5ebd2efa240a6196abd8c27547be7a76501a8c3
lucifer <lucifer116664@gmail.com>
2023-06-01 13:17:00 +0300

    lab4 and lab 5

--- src/Lab4/Lab4.java
+++ src/Lab4/Lab4.java
@@ -1,2 +1,66 @@
-package Lab4;public class Lab4 {
+package Lab4;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Lab4 {
+    final static long HEAD_SIZE = 120;
+    static int textSize = 0;
+
+    public static void main(String[] args) throws IOException {
+        encode();
+        decode();
+        System.out.println("Done.");
+    }
+
+    private static void encode() {
+        System.out.println("Encoding started...");
+        try (FileInputStream img = new FileInputStream("src/Lab4/resources/NULP.bmp");
+             FileInputStream msg = new FileInputStream("src/Lab4/resources/Message.txt");
+             FileOutputStream out = new FileOutputStream("src/Lab4/resources/EncodedImage.bmp");) {
+
+            int c, mb;
+            byte clearBit1 = (byte) 0xFE; // 254; // 11111110
+
+            for (int i = 1; i <= HEAD_SIZE; i++) out.write(img.read());
+
+            while ((mb = msg.read()) != -1) {
+                for (int bit = 7; bit >= 0; bit--) {
+                    c = img.read() & clearBit1;
+                    c = (c | ((mb >> bit) & 1));
+                    out.write(c);
+                }
+                textSize++;
+            }
+            for (int bit = 7; bit >= 0; bit--) {
+                out.write(img.read());
+            }
+            while ((c = img.read()) != -1) out.write(c);
+            System.out.println("Encoding was successfully finished.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void decode() {
+        System.out.println("Decoding started...");
+        try(FileInputStream img = new FileInputStream("src/Lab4/resources/EncodedImage.bmp");
+            FileOutputStream out = new FileOutputStream("src/Lab4/resources/DecodedMessage.txt")) {
+
+            for (int i = 1; i <= HEAD_SIZE; i++)
+                img.read();
+
+            byte[] result = new byte[textSize];
+            for (int i = 0; i < textSize; i++) {
+                for (int bit = 0; bit <= 7; bit++) {
+                    result[i] = (byte) ((result[i] << 1) | (img.read() & 1));
+                }
+                out.write(result[i]);
+            }
+            System.out.println("Decoding was successfully finished.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Lab5/Lab5.java
+++ src/Lab5/Lab5.java
@@ -1,2 +1,57 @@
-package Lab5;public class Lab5 {
+package Lab5;
+
+import javax.crypto.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+
+public class Lab5 {
+    public static void encrypt(SecretKey key) throws Exception {
+        FileOutputStream msgOut = new FileOutputStream("src/Lab5/resources/encryptedMsg.txt");
+
+        byte[] message = Files.readAllBytes(Paths.get("src/Lab5/resources/msg.txt"));
+
+        Cipher desCipher = Cipher.getInstance("DES");
+
+        desCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedMsg = desCipher.doFinal(message);
+
+        msgOut.write(encryptedMsg);
+        msgOut.flush();
+
+        System.out.println("Message was successfully encrypted!");
+        msgOut.close();
+    }
+
+    public static void decrypt(SecretKey key) throws Exception {
+
+        FileOutputStream msgOut = new FileOutputStream("src/Lab5/resources/decryptedMsg.txt");
+
+        byte[] encryptedMessage = Files.readAllBytes(Paths.get("src/Lab5/resources/encryptedMsg.txt"));
+
+        Cipher desCipher = Cipher.getInstance("DES");
+
+        desCipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedMsg = desCipher.doFinal(encryptedMessage);
+
+        msgOut.write(decryptedMsg);
+        msgOut.flush();
+
+        System.out.println("Message was successfully decrypted!");
+        msgOut.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+            encrypt(key);
+            decrypt(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
